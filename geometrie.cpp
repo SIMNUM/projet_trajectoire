fc6de15707713a2977db66ff50e249ce602c117f
emmanueljay <Manu@Holyshit.local>
2014-02-07 11:52:48 +0100

    Classe Sommet

--- geometrie.cpp
+++ geometrie.cpp
@@ -8,4 +8,61 @@
 
 #include "geometrie.h"
 
+/* FONCTIONS DE LA CLASSE SEGMENT */
 
+    // Constructeurs
+
+
+sommet::sommet(){ // Point de dimension 2 par défaut nul
+    dim=2;
+    pts = new double[2];
+    pts[0] = 0.;pts[1] = 0.;
+};
+sommet::sommet(double x){
+    dim=1;
+    pts = new double[1];
+    pts[0] = x;
+}
+sommet::sommet(double x,double y){
+    dim=2;
+    pts = new double[2];
+    pts[0] = x;pts[1] = y;
+}
+sommet::sommet(double x,double y,double z){
+    dim=2;
+    pts = new double[3];
+    pts[0] = x;pts[1] = y;pts[2]=z;
+}
+sommet::sommet(const sommet& s){
+    dim = s.dim;
+    pts = new double[dim];
+    for (int i=0; i<dim; i++) {
+        pts[i]=s.pts[i];
+    }
+}
+sommet& sommet::operator= (const sommet& s){
+    dim = s.dim;
+    pts = new double[dim];
+    for (int i=0; i<dim; i++) {
+        pts[i]=s.pts[i];
+    }
+    return *this;
+}
+
+    // Affichage
+
+std::ostream& operator <<(std::ostream & out,const sommet & s){
+    out<< "(" << s.pts[0];
+    for (int i=1; i<s.dim; i++) {
+        out << "," << s.pts[i];
+    };
+    out<< ")";
+    return out;
+}
+void sommet::print(){
+    std::cout<< "(" << pts[0];
+    for (int i=1; i<dim; i++) {
+        std::cout << "," << pts[i];
+    };
+    std::cout<< ")";
+}
--- geometrie.h
+++ geometrie.h
@@ -13,7 +13,23 @@
 
 class sommet {
     public :
-    double x,y;
+    int dim;
+    double* pts;
+// CONSTRUCTEURS
+    sommet(); // Point de dimension 2 par défaut nul
+    sommet(double x);
+    sommet(double x,double y);
+    sommet(double x,double y,double z);
+    sommet(const sommet& s);
+    sommet& operator= (const sommet& s);
+// AFFICHAGE
+    friend std::ostream& operator <<(std::ostream &,const sommet &);
+    void print();
+};
+
+class segment {
+    public :
+    sommet S1,S2;
 };
 
 #endif /* defined(__Projet_Xcode___Planification_de_trajectoire__geometrie__) */
