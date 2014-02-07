5105a15bb90f555765f93163a04ed43658710886
emmanueljay <Manu@Holyshit.local>
2014-02-07 11:00:54 +0100

    Premier ajout de fichier

---
+++ geometrie.cpp
@@ -0,0 +1,10 @@
+//
+//  geometrie.cpp
+//  Projet Xcode : Planification de trajectoire
+//
+//  Created by Emmanuel JAY on 07/02/2014.
+//  Copyright (c) 2014 Emmanuel JAY. All rights reserved.
+//
+
+#include "geometrie.h"
+
---
+++ geometrie.h
@@ -0,0 +1,14 @@
+//
+//  geometrie.h
+//  Projet Xcode : Planification de trajectoire
+//
+//  Created by Emmanuel JAY on 07/02/2014.
+//  Copyright (c) 2014 Emmanuel JAY. All rights reserved.
+//
+
+#ifndef __Projet_Xcode___Planification_de_trajectoire__geometrie__
+#define __Projet_Xcode___Planification_de_trajectoire__geometrie__
+
+#include <iostream>
+
+#endif /* defined(__Projet_Xcode___Planification_de_trajectoire__geometrie__) */
