f70b4332cf5940bf240d0ce1b8d5b02a503a55ea
emmanueljay <Manu@Holyshit.local>
2014-02-10 10:12:33 +0100

    Classes Sommet/Vecteur/Segment opérationnelles

--- geometrie.cpp
+++ geometrie.cpp
@@ -8,39 +8,39 @@
 
 #include "geometrie.h"
 
-/* FONCTIONS DE LA CLASSE SEGMENT */
+/* FONCTIONS DE LA CLASSE VECTEUR */
 
     // Constructeurs
 
 
-sommet::sommet(){ // Point de dimension 2 par défaut nul
+vecteur::vecteur(){ // Point de dimension 2 par défaut nul
     dim=2;
     pts = new double[2];
     pts[0] = 0.;pts[1] = 0.;
 };
-sommet::sommet(double x){
+vecteur::vecteur(double x){
     dim=1;
     pts = new double[1];
     pts[0] = x;
 }
-sommet::sommet(double x,double y){
+vecteur::vecteur(double x,double y){
     dim=2;
     pts = new double[2];
     pts[0] = x;pts[1] = y;
 }
-sommet::sommet(double x,double y,double z){
+vecteur::vecteur(double x,double y,double z){
     dim=3;
     pts = new double[3];
     pts[0] = x;pts[1] = y;pts[2]=z;
 }
-sommet::sommet(const sommet& s){
+vecteur::vecteur(const vecteur& s){
     dim = s.dim;
     pts = new double[dim];
     for (int i=0; i<dim; i++) {
         pts[i]=s.pts[i];
     }
 }
-sommet& sommet::operator= (const sommet& s){
+vecteur& vecteur::operator= (const vecteur& s){
     dim = s.dim;
     pts = new double[dim];
     for (int i=0; i<dim; i++) {
@@ -51,7 +51,7 @@ sommet& sommet::operator= (const sommet& s){
 
     // Affichage
 
-std::ostream& operator <<(std::ostream & out,const sommet & s){
+std::ostream& operator <<(std::ostream & out,const vecteur & s){
     out<< "(" << s.pts[0];
     for (int i=1; i<s.dim; i++) {
         out << "," << s.pts[i];
@@ -59,10 +59,201 @@ std::ostream& operator <<(std::ostream & out,const sommet & s){
     out<< ")";
     return out;
 }
-void sommet::print(){
+void vecteur::print(){
     std::cout<< "(" << pts[0];
     for (int i=1; i<dim; i++) {
         std::cout << "," << pts[i];
     };
     std::cout<< ")";
+}
+
+vecteur::vecteur(const sommet& s1,const sommet& s2){
+    if (s1.dim!=s2.dim) {
+        pts=NULL;
+        dim=0;
+    }
+    else{
+        dim = s1.dim;
+        pts = new double[dim];
+        for (int i=0; i<dim; i++) {
+            pts[i]=s2.pts[i]-s1.pts[i];
+        }
+    }
+}
+
+    // Combinaisons linéaires
+
+vecteur & vecteur::operator+=(const vecteur & v){
+    
+    for (int i=0; i<dim; i++) {
+        pts[i]+= v.pts[i];
+    };
+    
+    return *this;
+}
+
+vecteur & vecteur::operator+=(const double lambda){
+    
+    for (int i=0; i<dim; i++) {
+        pts[i]+= lambda;
+    };
+    
+    return *this;
+}
+
+vecteur & vecteur::operator-=(const vecteur & v){
+    
+    for (int i=0; i<dim; i++) {
+        pts[i]-= v.pts[i];
+    };
+    
+    return *this;
+}
+
+vecteur & vecteur::operator-=(const double lambda){
+    
+    for (int i=0; i<dim; i++) {
+        pts[i]-= lambda;
+    };
+    
+    return *this;
+}
+
+vecteur & vecteur::operator*=(const double lambda){
+    
+    for (int i=0; i<dim; i++) {
+        pts[i] = pts[i]*lambda;
+    };
+    
+    return *this;
+}
+
+vecteur & vecteur::operator/=(const double lambda){
+    
+    for (int i=0; i<dim; i++) {
+        pts[i] = pts[i]/lambda;
+    };
+    
+    return *this;
+}
+
+bool operator==(const vecteur & a,const vecteur & b){
+    
+    if (a.dim!=b.dim) {
+        return false;
+    }
+    
+    for (int i=0; (i<a.dim) ; i++) {
+        if (a.pts[i]!=b.pts[i]) {
+            return false;
+        }
+    };
+    return true;
+}
+
+bool operator!=(const vecteur & a,const vecteur & b){
+    
+    if (a.dim!=b.dim) {
+        return true;
+    }
+    
+    for (int i=0; (i<a.dim) ; i++) {
+        if (a.pts[i]==b.pts[i]) {
+            return false;
+        }
+    };
+    return true;
+}
+
+vecteur operator +(const vecteur &a,const vecteur &b){
+    vecteur somme(a);
+    somme+=b;
+    return somme;
+}
+vecteur operator +(const vecteur &a,const double & lambda){
+    vecteur somme(a);
+    somme+=lambda;
+    return somme;
+}
+vecteur operator +(const double & lambda,const vecteur &a){
+    vecteur somme(a);
+    somme+=lambda;
+    return somme;
+}
+vecteur operator -(const vecteur &a,const vecteur &b){
+    vecteur dif(a);
+    dif-=b;
+    return dif;
+}
+vecteur operator -(const vecteur &a,const double & lambda){
+    vecteur dif(a);
+    dif-=lambda;
+    return dif;
+}
+vecteur operator -(const double & lambda,const vecteur &a){
+    vecteur dif(a);
+    dif-=lambda;
+    return dif;
+}
+vecteur operator *(const vecteur &a,const double & lambda){
+    vecteur pr(a);
+    pr*=lambda;
+    return pr;
+}
+vecteur operator *(const double & lambda,const vecteur &a){
+    vecteur pr(a);
+    pr*=lambda;
+    return pr;
+}
+vecteur operator /(const vecteur &a,const double & lambda){
+    vecteur divi(a);
+    divi/=lambda;
+    return divi;
+}
+
+    // Fonctions Euclidiennes
+
+double ps ( const vecteur & a,const vecteur & b){
+    double prosca=0.;
+    for (int i=0; i<a.dim; i++) {
+        prosca+= a.pts[i]*b.pts[i];
+    };
+    return prosca;
+}
+
+double norm (const vecteur & v){
+    return (sqrt(ps(v,v)));
+}
+
+vecteur rotation_d2 (const vecteur& v, double theta){
+    vecteur vec(v);
+    if (v.dim!=2) {
+        vec.pts=NULL;
+        vec.dim = 0;
+        return vec;
+    }
+    else
+    {
+        vec.pts[0]= cos(theta)*v.pts[0] - sin(theta)*v.pts[1];
+        vec.pts[1]= sin(theta)*v.pts[0] + cos(theta)*v.pts[1];
+        return vec;
+    }
+}
+
+/* FONCTIONS DE LA CLASSE SEGMENT */
+
+    // Constructeurs
+
+segment::segment(const sommet& s1,const sommet& s2){
+    S1 = s1;
+    S2 = s2;
+    n = rotation_d2(vecteur(s1, s2), -PI/2);
+    n /= norm(n);
+}
+
+    // Affichage
+
+std::ostream& operator <<(std::ostream & out,const segment & s){
+    out<< "S1 : " << s.S1 <<" S2 : "<< s.S2 << " n : "<< s.n << "\n";
+    return out;
 }
--- geometrie.h
+++ geometrie.h
@@ -10,26 +10,70 @@
 #define __Projet_Xcode___Planification_de_trajectoire__geometrie__
 
 #include <iostream>
+#include <math.h>
 
-class sommet {
+const double PI= 3.1415926;
+
+class vecteur;
+typedef vecteur sommet;
+
+class vecteur {
     public :
     int dim;
     double* pts;
 // CONSTRUCTEURS
-    sommet(); // Point de dimension 2 par défaut nul
-    sommet(double x);
-    sommet(double x,double y);
-    sommet(double x,double y,double z);
-    sommet(const sommet& s);
-    sommet& operator= (const sommet& s);
+    vecteur(); // Point de dimension 2 par défaut nul
+    vecteur(double x);
+    vecteur(double x,double y);
+    vecteur(double x,double y,double z);
+    vecteur(const vecteur& s);
+    vecteur(const sommet& v1,const sommet& v2);
+    vecteur& operator= (const vecteur& s);
 // AFFICHAGE
-    friend std::ostream& operator <<(std::ostream &,const sommet &);
+    friend std::ostream& operator <<(std::ostream &,const vecteur &);
     void print();
+// COMBINAISONS LINEAIRES
+    vecteur & operator +=(const vecteur & v);
+    vecteur & operator +=(const double);
+    vecteur & operator -=(const vecteur & v);
+    vecteur & operator -=(const double);
+    vecteur & operator *=(const double);
+    vecteur & operator /=(const double);
+// ACCES
+    double& operator ()(int i) {return pts[i-1];}
+    double& operator [](int i) {return pts[i];}
+    double operator ()(int i) const{return pts[i-1];}
+    double operator [](int i) const{return pts[i];}
 };
+
+
+// FONCTIONS DE COMBINAISONS DE VECTEURS
+bool operator==(const vecteur & a,const vecteur & b);
+bool operator!=(const vecteur & a,const vecteur & b);
+vecteur operator +(const vecteur &,const vecteur &);
+vecteur operator +(const vecteur &,const double & lambda);
+vecteur operator +(const double & lambda,const vecteur &);
+vecteur operator -(const vecteur &,const vecteur &);
+vecteur operator -(const vecteur &,const double & lambda);
+vecteur operator -(const double & lambda,const vecteur &);
+vecteur operator *(const vecteur &,const double & lambda);
+vecteur operator *(const double & lambda,const vecteur &);
+vecteur operator /(const vecteur &,const double & lambda);
+
+// FONCTIONS EUCLIDIENNES
+double ps ( const vecteur & ,const vecteur &);
+double norm (const vecteur & v);
+vecteur rotation_d2 (const vecteur& v, double theta);
+
 
 class segment {
     public :
     sommet S1,S2;
+    vecteur n;
+    segment(const sommet& s1,const sommet& s2);
+// AFFICHAGE
+    friend std::ostream& operator <<(std::ostream &,const segment &);
 };
+
 
 #endif /* defined(__Projet_Xcode___Planification_de_trajectoire__geometrie__) */
